463254b0d8fd8fd507ab0f6fa8c88b89e8d57550
Eduardo Corrêa <correaeduardo96@outlook.com>
2023-11-06 20:31:41 -0300

    First components
    Primeiros componentes criados em Angular.

---
+++ first-app/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+        <h2>Meu primeiro component com Angular! (manual)</h2>
+    `
+})
+export class MeuPrimeiroComponent {}
